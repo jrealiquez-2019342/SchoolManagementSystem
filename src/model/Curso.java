ea363ecf49abf9ad8140a2f07f6e6e88aec24d79
Joshua Realiquez <jrealiquez-2019342@kinal.edu.gt>
2024-09-11 12:14:49 -0600

    modelo de curso v.1

---
+++ src/model/Curso.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Curso {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int numeroCreditos;
+    private int version;
+
+    public Curso() {
+    }
+
+    public Curso(int id, String nombre, String descripcion, int numeroCreditos, int version) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.numeroCreditos = numeroCreditos;
+        this.version = version;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getNumeroCreditos() {
+        return numeroCreditos;
+    }
+
+    public void setNumeroCreditos(int numeroCreditos) {
+        this.numeroCreditos = numeroCreditos;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
